71414318b1ae87f71f9611db2b27826b11fd99a5
daulat <daulatsingh7878@gmail.com>
2025-08-03 23:16:45 +0530

    change in index.js in Backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ import path from "path";
 
 dotenv.config()
 
-const PORT= process.env.PORT || 5001
+const PORT= process.env.PORT
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(cookieParser())
 app.use(
     cors({
-        // origin: "*",
         origin: "http://localhost:5173",
         credentials: true,
     }
@@ -31,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 })
 }

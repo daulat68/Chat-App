f2053584d9c8778df40ae4f2611922780d19cc35
daulat <daulatsingh7878@gmail.com>
2025-08-03 19:35:58 +0530

    make production ready

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,10 +6,12 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import cors from "cors"
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config()
 
 const PORT= process.env.PORT || 5001
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
@@ -25,6 +27,14 @@ app.use(
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+})
+}
 
 server.listen(5001, ()=>{
     console.log(`Server is listening on PORT: ${PORT}`)

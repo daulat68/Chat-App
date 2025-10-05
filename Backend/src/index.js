626b67434ecb545d2a74c7336563ce580389b0f3
daulat <daulatsingh7878@gmail.com>
2025-10-05 12:12:54 +0530

    add google verification

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,9 +17,13 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")));
+
+const allowedOrigin =
+    process.env.NODE_ENV === "production" ? "https://chat-app-jwzl.onrender.com" : "http://localhost:5173";
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: allowedOrigin,
         credentials: true,
     }
 ))
